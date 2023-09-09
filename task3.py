5427a3b92269aa4f5afc17886b6b20ec75f16813
farahbassiony <125823155+farahbassiony@users.noreply.github.com>
2023-09-09 13:51:19 +0300

    task 3

---
+++ task3.py
@@ -0,0 +1,58 @@
+class complex_number:
+    def __init__(self, number, real=0, imaginary=0):
+        self.number = number
+        self.real = real
+        self.imaginary = imaginary
+
+    def split_complex(self):
+        delimiter = "+" if "+" in self.number else "-"
+        real, imaginary = self.number.split(delimiter)
+        return real, imaginary.rstrip('i')  
+
+    def __add__(self, other):
+        real1, imag1 = self.split_complex()
+        real2, imag2 = other.split_complex()
+        real_sum = int(real1) + int(real2)
+        imag_sum = int(imag1) + int(imag2)
+        return complex_number("", real_sum, imag_sum)
+
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"
+    def __sub__(self, other):
+        real1, imag1 = self.split_complex()
+        real2, imag2 = other.split_complex()
+        real_dif = int(real1) - int(real2)
+        imag_dif = int(imag1) - int(imag2)
+        return complex_number("", real_dif, imag_dif)
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"
+    def __mul__ (self, other):
+        real1, imag1 = self.split_complex()
+        real2, imag2 = other.split_complex()
+        real_pro = int(real1) * int(real2)
+        imag_pro = int(imag1) * int(imag2)
+        return complex_number("", real_pro, imag_pro)
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"
+    def __truediv__  (self, other):
+        real1, imag1 = self.split_complex()
+        real2, imag2 = other.split_complex()
+        real_quo = float(real1) / float(real2)
+        imag_quo = float(imag1) / float(imag2)
+        return complex_number("", real_quo, imag_quo)
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"
+
+x = input("Enter the first complex number: ")
+y = input("Enter the second complex number: ")
+num1 = complex_number(x)  
+num2 = complex_number(y) 
+
+result_addition = num1 + num2
+print(result_addition)
+result_subtraction=num1-num2
+print(result_subtraction)
+result_multiplication=num1 * num2
+print(result_multiplication) 
+result_division=num1/num2
+print(result_division) 
